6c2eb4f154fddcf625b37eb1b7f14ce1efea0f0d
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-07 11:07:30 +0200

    step_4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var test1 = prompt("Enter your name:" );    
+var test2 = prompt("Enter your surname:");    
+var test3 = prompt("Enter your city:");    
+alert("Enter your name:" + ' ' + test1 + '\n' + "Enter your surname"  + ' ' + test2 + '\n' + "Enter your city:" + ' ' + test3);
