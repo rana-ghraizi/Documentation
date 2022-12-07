292ce8dd143e743ad4e791bab047afe3a578014d
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-07 13:14:21 +0200

    step_7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_7.js
@@ -0,0 +1,11 @@
+var test1 = prompt("Enter your shoe size:" ); 
+var test2 = prompt("Enter your birthday date:" ); 
+function myfunction(test1,test2) {
+    var x = test1 * 2;
+    var y = x + 5;
+    var a = y * 50;
+    var b = a - test2;
+    var result = b + 1766;
+return alert(result);
+}
+myfunction(test1,test2);
