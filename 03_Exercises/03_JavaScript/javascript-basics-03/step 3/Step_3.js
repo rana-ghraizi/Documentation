242cd510c31c13beb3ef0572c04c33c6b2d9eae0
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-08 21:40:34 +0200

    finished step_3 javascript-basic 03

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+function green(){
+        document.querySelector('#text').style.color = "green";
+    }
+function red(){
+        document.querySelector('#text').style.color = "red";
+    }
+function blue(){
+        document.querySelector('#text').style.color = "blue";
+    }
