fb75fea2cdcbaa50b03f29e3aaa33b719528820d
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-08 23:20:43 +0200

    finished half of javascript-basic 04

--- 03_Exercises/03_JavaScript/javascript-basics-04-tdd/questions.js
+++ 03_Exercises/03_JavaScript/javascript-basics-04-tdd/questions.js
@@ -1,63 +1,64 @@
 
 const stringSize = (text) => {
-
+    return text.length;
 }
 const replaceCharacterE = (text) => {
-
+    return text.replace("e", " ");
 }
 const concatString = (text1, text2) => {
-
+    return text1.concat(text2);
 }
 const showChar5 = (text) => {
-
+    return text.charAt(4);
 }
 const showChar9 = (text) => {
-
+    return text.slice(0, 9);
 }
 const toCapitals = (text) => {
-
+    return text.toUpperCase();
 }
 const toLowerCase = (text) => {
-
+    return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+    return text.trim();
 }
 const IsString = (text) => {
-
+    return text.IsString=true;
 }
 
 const getExtension = (text) => {
-
+    return text.split(".").pop();
 }
 const countSpaces = (text) => {
-
+    return text.split(' ').length - 1;
 }
 const InverseString = (text) => {
-
+    return text.split("").reverse().join("");
 }
 
 const power = (x, y) => {
-
+    return Math.pow(x, y);
 }
 const absoluteValue = (num) => {
-
+    return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
-
+    return array.map(Math.abs);
 }
 const circleSurface = (radius) => {
-
+    return Math.round(Math.PI * radius * radius);
 }
 const hypothenuse = (ab, ac) => {
-
+    return Math. sqrt(ab*ab + ac*ac);
 }
 const BMI = (weight, height) => {
-
+    return parseFloat((weight / (height * height)).toFixed(2)); 
 }
 
 const createLanguagesArray = () => {
-
+    const languages = ["Html", "CSS", "Java", "PHP"];
+  return languages;
 }
 
 const createNumbersArray = () => {
