61e92ddbe17b1319125decd237d801597f5901c5
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-09 22:52:01 +0200

    finished Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,8 @@
+1. select Name from students
+2. select Name from students where Age > "30";
+3. select Name from students where Gender = "F" AND Age = "30";
+4. select Points from students WHERE name = "Alex";
+5. INSERT INTO students(ID, Name, Age, Gender, Points) VALUES ("7", "Rana", "18", "F", "460");
+6. UPDATE students SET Points = "350" WHERE ID="2";
+7. UPDATE students SET Points = "150" WHERE ID="1";
+8. 
