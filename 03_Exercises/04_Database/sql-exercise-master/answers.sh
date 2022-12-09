eb2d4c207668142ab582bd474b37e4a447b24979
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-09 23:39:51 +0200

    finished Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -5,4 +5,9 @@
 5. INSERT INTO students(ID, Name, Age, Gender, Points) VALUES ("7", "Rana", "18", "F", "460");
 6. UPDATE students SET Points = "350" WHERE ID="2";
 7. UPDATE students SET Points = "150" WHERE ID="1";
-8. 
+8.
+9.
+10. INSERT INTO graduates (ID, Name, Age, Gender, Points) VALUES ("4", "Layal", "18", "F", "350");
+11. UPDATE graduates SET Graduation = "08/09/2018" WHERE ID="4";
+12. delete from students where ID="4";
+13. 
