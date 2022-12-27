b908dd7f4e1594eb8464a3bd9b2645142f5bfd5c
Rana Ghraizi <ranaghraizi1@gmail.com>
2022-12-27 20:45:59 +0200

    extended hello

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -34,17 +34,20 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n'|| text === 'exit\n') {
+  text = text.replace("\n", " ");
+  text = text.trim();
+  text = text.split(" ");
+  if (text[0] === 'quit'|| text[0] === 'exit') {
     quit();
   }
-  else if(text === 'hello\n'){
-    hello();
+  else if(text[0] === 'hello'){
+    hello(text[1]);
   }
-  else if(text === 'help\n'){
+  else if(text[0] === 'help'){
     help();
   }
   else{
-    unknownCommand(text);
+    unknownCommand(text[0]);
   }
 }
 
@@ -67,9 +70,10 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello(x){
+  console.log('hello ' +x+ '!')
 }
+
 // This function lists all the possible commands
 function help(){
   console.log('hello: says hello!')
