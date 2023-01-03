ccacc15252ca145814c7d4f285119e67beb54707
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-01-03 21:07:39 +0200

    step 4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -13,10 +13,24 @@ app.listen(port, () => {
 app.get('/test', (req, res) => {
     res.send({ status: 200, message: 'ok'});
 })  
-
 app.get('/time', (req, res) => {
     const currentTime = new Date();
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
     res.send({ status: 200, message: `${hours}:${minutes}`});
 })  
+app.get('/hello', (req, res) => {
+    res.send({ status: 200, message: 'Hello' });
+});
+app.get('/hello/:id', (req, res) => {
+    const id = req.params.id;
+    res.send({ status: 200, message: `Hello, ${id}` });
+});
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+    if (search) {
+        res.send({ status: 200, message: 'ok', data: search });
+    } else {
+        res.send({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+})
