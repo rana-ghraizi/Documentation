ce4894678275879edcbb89885245180a746938a6
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-01-03 21:33:55 +0200

    step 5

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -34,3 +34,21 @@ app.get('/search', (req, res) => {
         res.send({ status: 500, error: true, message: 'you have to provide a search' });
     }
 })
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+app.get('/movies/create', (req, res) => { 
+    res.send({ });
+});
+app.get('/movies/read', (req, res) => {
+    res.send({ status: 200, data: movies});
+})
+app.get('/movies/update', (req, res) => { 
+    res.send({ });
+});
+app.get('/movies/delete', (req, res) => {
+    res.send({ });
+})
