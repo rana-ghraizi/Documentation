5a5640a490aff7bdaded9f2d7589630c498a225b
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-01-03 13:42:54 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -9,3 +9,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
+
+app.get('/test', (req, res) => {
+    res.send({ status: 200, message: 'ok'});
+})  
+
+app.get('/time', (req, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    res.send({ status: 200, message: `${hours}:${minutes}`});
+})  
